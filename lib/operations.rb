67749ff8d55f5d952f3bd5f53efd77bfaef8dc52
Antony Sanders <antony.devon92@gmail.com>
2019-11-07 17:12:21 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,8 +11,15 @@ end
 
 
 def not_safe?(speed)
-	speed = 50
-	speed > 60 ? true : false
+  
+  if speed > 60
+    return true
+    elsif speed < 40
+    return true
+  else speed = 50 
+    speed > 60 ? true : false
+  end
+
 end
 	
 
