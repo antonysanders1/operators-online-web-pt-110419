66d8fe825d4ebf33e516c918dad5d0e646f4065c
Antony Sanders <antony.devon92@gmail.com>
2019-11-07 16:54:07 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,8 @@
 def unsafe?(speed)
-  speed > 60 ? true : false
-  speed < 60 ? true : false
+  if speed > 60 ? true : false
+    elsif speed < 60 ? true : false
+    elsif speed > 60 ? true : false
+  end
   
 
 end
